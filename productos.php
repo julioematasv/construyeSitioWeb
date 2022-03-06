5aed43b068e00040b36edbf5d9c3ae9e8000df88
julio mata <julioematasv@gmail.com>
2022-03-06 12:42:02 +0100

    Iniciando Repositorio con algunos archivos.

---
+++ productos.php
@@ -0,0 +1,63 @@
+<?php include('./template/cabecera.php'); ?>
+
+    <!-- desde aquí el template del contenido  -->
+
+
+
+        <div class="col-md-3">
+
+          <div class="card mx-1">
+
+            <img class="card-img-top" src="./img/img01.jpg" alt="Libro1">
+
+            <div class="card-body">
+              <h3 class="card-title">Libro PHP</h3>
+              <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+
+          <div class="card mx-1">
+            
+            <img class="card-img-top" src="./img/img02.jpg" alt="Libro2">
+
+            <div class="card-body">
+              <h3 class="card-title">Libro PHP</h3>
+              <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+
+          <div class="card mx-1">
+            
+            <img class="card-img-top" src="./img/img03.jpg" alt="Libro3">
+
+            <div class="card-body">
+              <h3 class="card-title">Libro PHP</h3>
+              <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+
+          <div class="card mx-1">
+            
+            <img class="card-img-top" src="./img/img04.jpg" alt="Libro4">
+
+            <div class="card-body">
+              <h3 class="card-title">Libro PHP</h3>
+              <a name="" id="" class="btn btn-primary" href="#" role="button">Ver Más</a>
+            </div>
+          </div>
+        </div>
+
+
+
+      <!-- hasta aquí el template del contenido  -->
+
+<?php include('./template/footer.php'); ?>
---
+++ template/footer.php
@@ -0,0 +1,13 @@
+   
+
+    </div>
+
+  </div>
+   <!-- desde aquí el template del footer  -->
+   <div class="row">
+        <div class="col-12 bg-dark text-white text-lg-center pt-3">
+          <p>Footer Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+</body>
+</html>
