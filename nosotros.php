27e9dc1b0cabd7d38e45f6365de82d93b3d9e71b
julio mata <julioematasv@gmail.com>
2022-03-06 12:56:17 +0100

    haciendo contenido de página Nosotros

--- nosotros.php
+++ nosotros.php
@@ -6,10 +6,7 @@
         <h1 class="display-3">Sobre Nosotros</h1>
         <p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, quidem.</p>
         <hr class="my-2">
-        <p>More info</p>
-        <p class="lead">
-          <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
-        </p>
+        
       </div>
 
       <!-- hasta aquí el template del contenido  -->
