193522e02d38b8c29d5b073828d8c3912a188f50
julio mata <julioematasv@gmail.com>
2022-03-08 09:57:58 +0100

    Dando forma al interfaz del formulario.

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,11 +1,54 @@
 <?php include("../template/cabecera.php");  ?>
 
-    <div class="col-md-5">
-      Formulario de libros
-    </div>
+    <div class="col-md-5"><!-- Inicio col md 5  -->
 
-    <div class="col-md-7">
+
+      <div class="card"><!-- Inicio del card  -->
+
+        <div class="card-header bg-secondary text-white"><!-- Inicio del card header  -->
+          Datos de Libro:
+        </div><!-- final del card header  -->
+
+        <div class="card-body"><!-- Inicio del card body  -->
+
+          <form method="POST" enctype="multipart/form-data" role="form" class="shadow px-3 py-3"><!-- Inicio del formulario  -->
+
+            <div class = "form-group"><!-- Inicio primer form group  -->
+              <label for="txtID">ID:</label><!--   -->
+              <input type="text" class="form-control" id="txtID" placeholder="Enter ID"><!--   -->
+            </div><!-- final primer form group  -->
+
+            <div class="form-group"><!-- Inicio segundo form group  -->
+              <label for="txtNombre">Nombre:</label>
+              <input type="text" class="form-control" name="txtNombre" id="txtNombre" placeholder="Nombre de Libro">
+            </div><!-- final segundo form group  -->
+
+            <div class="form-group"><!-- Inicio tercer form group  -->
+              <label for="txtImagen">Imagen:</label>
+              <input type="file" class="form-control" name="txtImagen" id="txtImagen">
+            </div><!-- final tercer form group  -->
+
+            <div class="btn-group" role="group" aria-label=""><!-- Inicio Botonera  -->
+              <button type="button" class="btn btn-success">Agregar</button>
+              <button type="button" class="btn btn-warning">Modificar</button>
+              <button type="button" class="btn btn-info">Cancelar</button>
+            </div><!-- final Botonera  -->
+
+          </form><!-- Final del formulario  -->
+
+        </div><!-- final del card body  -->
+
+        <div class="card-footer bg-secondary text-white"><!-- Inicio del card footer  -->
+          Desde aquí gestionas los datos de los libros.
+        </div><!-- final del card footer  -->
+
+      </div><!-- final del card  -->
+      
+      
+    </div><!-- final col md 5  -->
+
+    <div class="col-md-7"><!-- Inicio col md 7  -->
       tabla con libros
-    </div>
+    </div><!-- final col md 7  -->
 
 <?php include("../template/footer.php");  ?>
