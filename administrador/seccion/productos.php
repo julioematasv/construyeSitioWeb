f0f3e76be93952cb6239d89c270c033cfbcb9ffd
julio mata <julioematasv@gmail.com>
2022-03-08 08:51:19 +0100

    Preparando la seccion de CRUD

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -1,5 +1,11 @@
 <?php include("../template/cabecera.php");  ?>
 
-        Libros
+    <div class="col-md-5">
+      Formulario de libros
+    </div>
+
+    <div class="col-md-7">
+      tabla con libros
+    </div>
 
 <?php include("../template/footer.php");  ?>
