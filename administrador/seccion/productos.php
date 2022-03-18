ebaee549400869b9570bd670355329d5ef8e0248
julio mata <julioematasv@gmail.com>
2022-03-18 08:43:30 +0100

    Corrección de error en linea 17 falta ;

--- administrador/seccion/productos.php
+++ administrador/seccion/productos.php
@@ -14,7 +14,7 @@ switch ($accion)
     $sentenciaSQL = $conexion->prepare("INSERT INTO libros (nombre,imagen) VALUES (:nombre,:imagen);");
     $sentenciaSQL->bindParam(':nombre',$txtNombre);
 
-    $fecha = new DateTime($sentenciaSQL->)
+    $fecha = new DateTime();
     $nombreArchivo=($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES["txtImagen"]["name"]:"imagen.jpg";
 
     $tmpImagen=$_FILES["txtImagen"]["tmp_name"];
